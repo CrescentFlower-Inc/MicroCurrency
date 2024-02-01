7c3f8c6c9b498f2aef3d531f5f2f9bfb03741d91
BadPythonCoder <xsare435@gmail.com>
2024-02-01 14:39:45 +0100

    Tribute the now removen testing code

--- microcurrency/__init__.py
+++ microcurrency/__init__.py
@@ -40,19 +40,6 @@ async def on_ready():
     await bot.tree.sync(), print(f"All commands has been synced!")
     print("Ready!")
 
-@bot.tree.command(name="test",description="A testing command for debugging bot.")
-async def test(interaction: discord.Interaction):
-    await interaction.response.send_message("[SUS](https://youtube.com/)")
-
-
-@bot.tree.command(name="embtest",description="A command for testing embeds.")
-async def embtest(interaction: discord.Interaction):
- embed=discord.Embed(title="Insert title here", description="It will be a testing embed.", color=0x6699ff)
- embed.add_field(name="Field1", value="No commands here", inline=True)
- embed.add_field(name="Field2", value="There's also no command", inline=True)
- embed.set_footer(text="Magest1ckkz")
- await interaction.response.send_message(embed=embed)
-
 @bot.tree.command(name="help",description="List of all commands available on the bot.")
 async def help(interaction: discord.Interaction):
  embed=discord.Embed(title="MicroCurrency [In-Development]", description="An economy bot that allows currency trades and exchanges within micronations.", color=0x6699ff)
@@ -67,23 +54,6 @@ async def embtest(interaction: discord.Interaction):
  embed=discord.Embed(title="MicroCurrency Rules", description="You must agree with the rules to use this bot. By using this bot, you agree with the rules.\n\n:one: • Do not use macros, other bots, or anything else for farming.\n:two: • No scamming\n:three: • No hacking", color=0x6699ff)
  embed.set_footer(text="Violating any of the rules might result to your bank account reset or terminated.")
  await interaction.response.send_message(embed=embed)
- 
-@bot.tree.command(name="exchangerates",description="View the current exchange rates.")
-async def embtest(interaction: discord.Interaction):
- embed=discord.Embed(title="Exchange rates (" + datetime.today().strftime('%d.%m.%Y')+")", description="`1` Lunarian Spilling (Ł) • `0,475` Pur (Ᵽ)", color=0x6699ff)
- embed.set_footer(text="This bot is in the very early stage of development, so exchange rates will not change.")
- await interaction.response.send_message(embed=embed)
-
-@bot.tree.command(name="strtest",description="A command for testing strings.")
-@app_commands.describe(user = "The user where it will be targeted to.", amount = "Amount of currency you want to give.", currency = "What currency to give to the user.")
-async def strtest(interaction: discord.Interaction, user: discord.Member, amount: int, currency: str):
-
-    embed1=discord.Embed(description=":x: Hey man, you can't send a currency that is 0 or a negative value.", color=0xff0000)
-    embed2=discord.Embed(description=":x: Hey man, sounds very stupid to send a currency to yourself!", color=0xff0000)
-
-    if amount <= 0: await interaction.response.send_message(embed=embed1),
-    elif user == interaction.user.mention: await interaction.response.send_message(embed=embed2),
-    else: await interaction.response.send_message(f"This is a testing command, representing {interaction.user.name}, that wanted to give {amount} {currency} to {user.mention}.")
 
 @app_commands.describe(currency = "What currency", user = "The target user")
 @app_commands.choices(currency = curchoices)
@@ -104,7 +74,7 @@ async def transfer(interaction: discord.Interaction, currency: app_commands.Choi
 		f":white_check_mark: Succesfully transfered {amount} {currency.symbol} to {int(user.display_name)}!",
 		":x: You cannot send no or negative money!",
 		":x: You cannot send money to yourself!",
-		":x: Insufficient funds"
+		":x: Insuffici;ent funds"
 	]
 
 	await interaction.response.send_message(respones[status])
