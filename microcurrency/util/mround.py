857558f1401271ed435499852e1a7fbd5e95c443
coderlol <xsare435@gmail.com>
2024-03-13 15:22:03 +0100

    Fix mround

--- microcurrency/util/mround.py
+++ microcurrency/util/mround.py
@@ -1,7 +1,9 @@
 def mround(x):
 	rounded = str(round(x, 2))
 
-	if len(rounded.split('.')[1]) == 1:
+	if len(rounded.split('.')) == 1:
+		rounded += ".00"
+	elif len(rounded.split('.')[1]) == 1:
 		rounded += "0"
 
 	return rounded
